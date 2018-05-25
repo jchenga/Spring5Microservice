fdc8b842c2e89baa39fc93b5dc5e5c4ecbcf5f10
Jack Cheng <j1cheng@bbyfs.ca>
2018-05-25 16:04:14 -0700

    search api gateway

---
+++ search-apigateway/src/main/java/com/acme/searchapigateway/SearchApigatewayApplication.java
@@ -0,0 +1,12 @@
+package com.acme.searchapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApigatewayApplication.class, args);
+	}
+}
