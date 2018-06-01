fd93de8e338925e04f374ba7cd2dec05b27f2a20
Jack Cheng <j1cheng@bbyfs.ca>
2018-06-01 16:26:59 -0700

    FareGatewayApplication

---
+++ fare-gateway/src/test/java/com/brownfield/faregateway/FareGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brownfield.faregateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FareGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
--- search-apigateway/src/main/java/com/acme/searchapigateway/SearchApiGateway.java
+++ search-apigateway/src/main/java/com/acme/searchapigateway/SearchApiGateway.java
@@ -4,13 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@CrossOrigin
 public class SearchApiGateway {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchApiGateway.class, args);
+	}
+
+}
+
+
+@RestController
+class SearchAPIGatewayController {
+
+	@RequestMapping("/")
+	String greet(HttpServletRequest req) {
+		return "<H1>Search Gateway Powered by Zuul</H1>";
 	}
 }
